15f328711d0646bcf88a40db2a1ac4ac5f19d5a1
lassejon <lasss@live.dk>
2022-02-22 13:10:26 +0100

    solved exercises 4.1-4.2

--- app.js
+++ app.js
@@ -1,55 +1,28 @@
-const http = require('http')
+
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstackopen:dan81hnw27@cluster0.cinyi.mongodb.net/BlogApp?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+logger.info('Trying to connect to:', config.MONGODB_URI)
+logger.info('...')
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log("connected")
+    logger.info('Connected')
   })
   .catch(() => {
-    console.log("not connected")
+    logger.error('Error: Could not connect')
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
